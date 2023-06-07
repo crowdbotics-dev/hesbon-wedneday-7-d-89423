7fd552ae1fedf636399528f01122500b62cd844e
Hesbon Kiptoo <hesbon.kiptoo@crowdbotics.com>
2023-06-07 12:39:21 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "hesbon-wedneday-7-d-89423",
-  url: "https://hesbon-wedneday-7-d-89423.botics.co",
-  api: "https://hesbon-wedneday-7-d-89423.botics.co/api/v1"
+  "name": "hesbon-wedneday-7-d-89423",
+  "url": "https://hesbon-wedneday-7-d-89423.botics.co",
+  "api": "https://hesbon-wedneday-7-d-89423.botics.co/api/v1",
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
